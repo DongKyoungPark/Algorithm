425bff6fa18e56e91c963e588293adc9e74ba241
DongKyoungPark <oe1lolo@nate.com>
2019-11-20 14:34:05 +0900

    크루스칼 알고리즘

---
+++ Algorithm/Kruskal_Algorithm.cpp
@@ -0,0 +1,15 @@
+// 크루스칼 알고리즘
+// 가장 적은 비용으로 모든 노드를 연결하기 위해 사용하는 알고리즘
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+
+	
+
+	return 0;
+}
